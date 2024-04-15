770a3d7bd974cd4e42d7270336e7eda268c96c6b
Leonardo Vieira <leonardovieira1993@gmail.com>
2024-04-15 16:22:09 -0300

    Primeiro commit, criação aplicação Spring

---
+++ M1S08/src/test/java/com/br/fullstackeducationM1S08/M1S08ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.fullstackeducationM1S08;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M1S08ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
